f31d0aecceb53985dc31d9c4f67e4d0ea2ea3698
Samuel Slade <12849513+sladesamuel@users.noreply.github.com>
2024-12-09 11:23:42 +0000

    Add random cards to collection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,58 @@
-import { memo } from "react"
+"use client"
 
-const Home = () => (
-  <div>TODO</div>
+import { getRandomCards } from "@/temp/database"
+import { memo, useState } from "react"
+
+type CardProps = {
+  name: string
+}
+
+const Card = ({ name }: CardProps) => (
+  <p>{name}</p>
 )
+
+type CollectionProps = {
+  cards: string[]
+}
+
+const Collection = ({ cards }: CollectionProps) => {
+
+  if (cards.length === 0) {
+    return <p>Your collection is empty. Open a pack to start collecting!</p>
+  }
+
+  return (
+    <>
+      {cards.map((cardName) => <Card key={cardName} name={cardName} />)}
+    </>
+  )
+}
+
+const Home = () => {
+  const [cards, setCards] = useState<string[]>([])
+  const [openingPack, setOpeningPack] = useState(false)
+
+  const openPack = () => {
+    setOpeningPack(true)
+
+    const newCards = getRandomCards(11)
+    setCards([...cards, ...newCards])
+
+    setOpeningPack(false)
+  }
+
+  return (
+    <>
+      <div>Welcome!</div>
+
+      <button onClick={openPack} disabled={openingPack}>
+        Open pack
+      </button>
+
+      <h1>Collection</h1>
+      <Collection cards={cards} />
+    </>
+  )
+}
 
 export default memo(Home)
